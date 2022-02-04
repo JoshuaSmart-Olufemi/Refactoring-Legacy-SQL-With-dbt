7b8fc3f2dee7d06a38b7547a8b75ab9a68cfe170
Joshua Smart <joshuasmartolufemi@gmail.com>
2022-02-04 13:07:07 +0100

    commit

--- dbt_refactorization/models/orders_mart/int_customer_orders.sql
+++ dbt_refactorization/models/orders_mart/int_customer_orders.sql
@@ -1,12 +1,14 @@
-customer_orders as (
+with int_customer_orders as (
 
-    select customers.id as customer_id
+    select 
+    customers.id as customer_id
     , min(order_date) as first_order_date
     , max(order_date) as most_recent_order_date
     , count(orders.id) AS number_of_orders
 from {{ ref ('stg_jaffle_shop_customers')}} as customers
 left join {{ ref('stg_jaffle_shop_orders') }} as orders
-on orders.user_id = customers.id 
-group by 1)
+on customers.id = orders.user_id 
+group by 1 
+)
 
-select * from customer_orders
+select * from int_customer_orders
