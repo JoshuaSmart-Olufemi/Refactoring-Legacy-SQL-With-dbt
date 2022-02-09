d525e719b1ec825908a75dc530c2fc11f4d41774
Joshua Smart <joshuasmartolufemi@gmail.com>
2022-02-09 15:47:48 +0100

    run-operation for dbt_audit_helper

--- dbt_refactorization/models/orders_mart/int_customer_orders.sql
+++ dbt_refactorization/models/orders_mart/int_customer_orders.sql
@@ -2,8 +2,8 @@ with int_customer_orders as (
 
     select 
     customers.id as customer_id
-    , min(order_date) as first_order_date
-    , max(order_date) as most_recent_order_date
+    , min(orders.order_date) as first_order_date
+    , max(orders.order_date) as most_recent_order_date
     , count(orders.id) AS number_of_orders
 from {{ ref ('stg_jaffle_shop_customers')}} as customers
 left join {{ ref('stg_jaffle_shop_orders') }} as orders
