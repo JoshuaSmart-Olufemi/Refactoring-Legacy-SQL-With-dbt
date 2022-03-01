97c078a79ee7b1993fab918c9e59288b333602b8
Joshua Smart <joshuasmartolufemi@gmail.com>
2022-03-01 14:03:02 +0100

    change in analyses folder

--- dbt_refactorization/analyses/compare_queries.sql
+++
@@ -1,8 +0,0 @@
-{% set old_etl_relation=ref('customer_orders')%}
-{% set dbt_relation=ref('customer_order_changes')%}
-
-{{ audit_helper.compare_relations(
-    a_relation=old_etl_relation,
-    b_relation=dbt_relation,
-    primary_key="order_id"
-) }}
